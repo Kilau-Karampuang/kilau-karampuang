8f3890152aa656ce5d228d50d1753812af2bb194
Nc Galih <encegalih@gmail.com>
2024-06-25 00:28:04 +0700

    feat: highlight image

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 const Hero = ({heading, subheading}) => {
     return (
-      <div className="flex relative items-center justify-center h-screen bg-gray-100">
+      <div className="flex relative items-center justify-center h-screen bg-white overflow-hidden">
         <div className="text-center z-10">
             <h2 className="text-5xl font-shadows -mb-3">
                 {heading}
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Shadows+Into+Light&display=swap" rel="stylesheet" />
       </Head>
-      <body>
+      <body className="overflow-x-hidden bg-white">
         <Main />
         <NextScript />
       </body>
