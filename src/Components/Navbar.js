f0d831d484ad8f421d653f4a6eb258895e388e25
Nc Galih <encegalih@gmail.com>
2024-07-07 12:48:10 +0700

    feat: dropdown navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 const NavItem = ({ href, label }) => {
     return (
@@ -15,16 +17,26 @@ const Navbar = () => {
         { href: '/umkm', label: 'UMKM' },
         { href: '/peta', label: 'Peta' },
         { href: '/makanan', label: 'Makanan'},
-        { href: '/sttunting-calculator', label: 'Stunting Calculator'}
     ];
 
     return (
       <div className="fixed w-full z-50">
-        <nav className="mt-6 mx-10 px-6 py-2 bg-white text-black font-montserrat bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-full flex justify-end items-center">
-          <div className="flex space-x-8">
+        <nav className="mt-6 mx-10 px-6 bg-white text-black font-montserrat bg-opacity-50 backdrop-filter backdrop-blur-lg rounded-full flex justify-between items-center">
+          <Link href="/">
+            <Image src="/Logo.png" width={30} height={30} />
+          </Link>
+          <div className="flex space-x-8 py-2">
               {links.map((link, index) => (
                   <NavItem key={index} href={link.href} label={link.label} />
               ))}
+              <div className="relative group text-black">
+                <button className="text-black">Layanan</button>
+                <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <Link href="/stunting-calculator" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">
+                    Stunting Calculator
+                  </Link>
+                </div>
+              </div>
           </div>
         </nav>  
       </div>
