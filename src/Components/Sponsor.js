f967f68b31b15839e89b4695c16a8ccc1ad3f7b4
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2024-07-25 11:49:12 +0800

    fix: styling on sponsor

--- src/Components/Sponsor.js
+++ src/Components/Sponsor.js
@@ -7,7 +7,7 @@ export default function Sponsor() {
         <h1 className="text-2xl font-montserrat">
           <span className="font-bold">Mitra</span> Kegiatan
         </h1>
-        <p className="text-lg font-montserrat">Kegiatan KKN PPM UGM 2024 Kilau Karampuang didukung oleh</p>
+        <p className="text-lg font-montserrat mb-4">Kegiatan KKN PPM UGM 2024 Kilau Karampuang didukung oleh</p>
         <div className="flex flex-col items-center mb-12 gap-4">
           <Image src="/bpjs-kesehatan.jpg" width={450} height={150} />
           <div className="flex flex-row justify-center items-center gap-4">
