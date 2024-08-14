0926e134f614df94d65a7518e7462ae002083e03
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2024-08-14 12:41:44 +0800

    fix: adding more img listg

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -17,11 +17,23 @@ const Gallery = () => {
           <img src="./highlight/4.png" alt="Image 1" className="h-28 w-auto mr-4" />
           <img src="./highlight/5.png" alt="Image 1" className="h-28 w-auto mr-4" />
           <img src="./highlight/6.png" alt="Image 1" className="h-28 w-auto mr-4" />
+          <img src="./highlight/1.png" alt="Image 1" className="h-28 w-auto mr-4" />
+          <img src="./highlight/2.png" alt="Image 1" className="h-28 w-auto mr-4" />
+          <img src="./highlight/3.png" alt="Image 1" className="h-28 w-auto mr-4" />
+          <img src="./highlight/4.png" alt="Image 1" className="h-28 w-auto mr-4" />
+          <img src="./highlight/5.png" alt="Image 1" className="h-28 w-auto mr-4" />
+          <img src="./highlight/6.png" alt="Image 1" className="h-28 w-auto mr-4" />
         </div>
       </div>
 
       <div className="overflow-hidden whitespace-nowrap">
         <div className="inline-flex animate-marquee-fast">
+          <img src="./highlight/3.png" alt="Image 2" className="h-28 w-auto mr-4" />
+          <img src="./highlight/4.png" alt="Image 2" className="h-28 w-auto mr-4" />
+          <img src="./highlight/5.png" alt="Image 2" className="h-28 w-auto mr-4" />
+          <img src="./highlight/6.png" alt="Image 2" className="h-28 w-auto mr-4" />
+          <img src="./highlight/1.png" alt="Image 2" className="h-28 w-auto mr-4" />
+          <img src="./highlight/2.png" alt="Image 2" className="h-28 w-auto mr-4" />
           <img src="./highlight/3.png" alt="Image 2" className="h-28 w-auto mr-4" />
           <img src="./highlight/4.png" alt="Image 2" className="h-28 w-auto mr-4" />
           <img src="./highlight/5.png" alt="Image 2" className="h-28 w-auto mr-4" />
